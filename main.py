c1dc390730ae24adeaa9a10af47492a176f75eab
Amrit Rai <iamamrit27@gmail.com>
2024-03-30 13:09:16 +0530

    add decoder

--- main.py
+++ main.py
@@ -106,3 +106,20 @@ class Encoder(nn.module):
             out = layer(out, out, out, mask)
 
         return out
+
+
+class Decoder(nn.module):
+    def __init__(self, embed_size, heads, forward_expansion, dropout, device):
+        super(Decoder, self).__init__()
+        self.attention = SelfAttention(embed_size, heads)
+        self.norm = nn.LayerNorm(embed_size)
+        self.transformer_block = TransformerBlock(
+            embed_size, heads, dropout, forward_expansion
+        )
+        self.dropout = nn.Dropout(dropout)
+
+    def forward(self, x, value, key, src_mask, trg_mask):
+        attention = self.attention(x, x, x, trg_mask)
+        query = self.dropout(self.norm1(attention + x))
+        out = self.transformer_block(value, key, query, src_mask)
+        return out
